c37a31c0ea89c41225a218c9c6badde2d2e84e24
Edrop7 <samovalle878@gmail.com>
2025-03-25 22:25:24 +0000

    created robot_heading_as_vector .cpp and executable in exercises_unit_2 dir to demo vector dynamic functionality

---
+++ exercises_unit_2/src/robot_heading_as_vector.cpp
@@ -0,0 +1,36 @@
+#include "robot_commander/robot_commander.h"
+#include <iostream>
+#include <ros/ros.h>
+#include <vector>
+
+int main(int argc, char **argv) {
+
+  ros::init(argc, argv, "heading_vector");
+
+  RobotCommander my_robot;
+
+  my_robot.move_in_circles();
+
+  int values_to_capture;
+  std::cout << "how many values should be captured: ";
+  std::cin >> values_to_capture;
+
+  std::vector<float> headings;
+  float captured_heading;
+
+  for (int i = 0; i < values_to_capture; i++) {
+    captured_heading = my_robot.get_heading();
+    headings.push_back(captured_heading);
+    std::cout << "Collecting heading value: " << captured_heading << std::endl;
+    ros::Duration(1.0).sleep();
+  }
+
+  std::cout << std::endl << "Displaying saved Data:" << std::endl;
+  for (int i = 0; i < values_to_capture; i++) {
+    std::cout << "Heading value " << i << ": " << headings.at(i) << std::endl;
+  }
+
+  my_robot.stop_moving();
+
+  return 0;
+}
