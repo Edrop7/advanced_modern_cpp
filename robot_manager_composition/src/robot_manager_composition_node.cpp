6734a71dbe3929473b40e57b4eea35437cb1ed8d
Edrop7 <samovalle878@gmail.com>
2025-03-28 15:10:05 +0000

    created robot_manager_composition pkg to demo class composition by providing a service to enable or disable console output

---
+++ robot_manager_composition/src/robot_manager_composition_node.cpp
@@ -0,0 +1,9 @@
+#include "robot_manager_composition/robot_manager_composition.h"
+#include <ros/ros.h>
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "robot_manager_using_composition");
+  ros::NodeHandle nh;
+  RobotManagerComposition rm = RobotManagerComposition(&nh);
+  ros::spin();
+}
